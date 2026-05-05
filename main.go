1b801fe172567401f0fa30fecaa4d93c4fddae88
my-app-s <94853425+my-app-s@users.noreply.github.com>
2026-05-05 23:58:23 +0000

    feat: implement generateRandomElements function

--- main.go
+++ main.go
@@ -11,7 +11,19 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		return []int{}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	result := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		result[i] = rand.Intn(100)
+	}
+
+	return result
 }
 
 // maximum returns the maximum number of elements.
